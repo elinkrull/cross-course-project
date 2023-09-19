56ca42feb159c718171597dca4a1782b6f9c9c1e
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2023-09-19 13:19:33 +0200

    added code to display jackets in product.html page in index.js, some changes in classes, ID´s and styling for the same functions in index.js

--- js/index.js
+++ js/index.js
@@ -1,34 +1,25 @@
+//  API call
+
 const urlRainyDays = "https://api.noroff.dev/api/v1/rainy-days";
 
-async function makeApiCall() {
-  try {
+async function getJackets() {
     const response = await fetch(urlRainyDays);
     const result = await response.json();
     return result;
-  } catch (error) {
-    console.log(error);
-    alert(error);
-  }
 }
-
-makeApiCall();
-
-export { makeApiCall };
-
-
 
 // Display the jackets on the index.html page:
 
 async function displayJackets() {
-  const jackets = await makeApiCall();
-  console.log(jackets);
+  const jackets = await getJackets() 
+  const jacketsFrontPageContainer = document.getElementById("best-sellers-jackets");
 
-  const jacketFrontPageContainer = document.getElementById("best-sellers-jackets");
 
   for (let i = 0; i < 4; i++) {
-    console.log(jackets[i]);
+    const jacket = jackets[i];
+    console.log(jacket);
 
-    jacketFrontPageContainer.innerHTML += `<div class=".best-sellers-jackets-container">
+    jacketsFrontPageContainer.innerHTML += `<div class="best-sellers-jackets-container">
                                         <a href="specificproduct.html?id=${jackets[i].id}"><img src="${jackets[i].image}" alt="${jackets[i].description}" class="images-js"></a>
                                         <h2>${jackets[i].title}</h2>
                                         <p>USD ${jackets[i].price}</p>
@@ -37,3 +28,33 @@ async function displayJackets() {
 }
 
 displayJackets();
+
+// Display jackets on product.html page
+
+
+async function displayAllJackets() {
+  const productJackets = await getJackets();
+  console.log(productJackets);
+
+const jacketsProductPageContainer = document.getElementById("all-jackets");
+
+for (let i = 0; i < 8; i++) {
+  const allJackets = productJackets[i];
+  console.log(allJackets);
+
+
+  jacketsProductPageContainer.innerHTML += `<div class=("all-jackets-container")>
+                                           <a href="specificproduct.html?id=${productJackets[i].id}"><img src="${productJackets[i].image}" alt="${productJackets[i].description}" class="images-js"></a>
+                                          <h2>${productJackets[i].title}</h2>
+                                          <p>USD ${productJackets[i].price}</p>
+                                          </div>`;
+ }
+}
+
+displayAllJackets();
+
+
+
+
+//can you explain more why you did the appendchild thing in the code in martes ca?
+
--- js/products.js
+++
@@ -1,2 +0,0 @@
-import { makeApiCall } from "./index.js";
-
